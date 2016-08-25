308bffbe067d17d2f0110052b25f2286d65acf08
Popoto <wandersonalvesferreira2012@gmail.com>
2016-08-24 23:07:56 -0300

    Trying CD to Heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express"),
     app = express(),
     path = require("path");
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.use('/res', express.static(__dirname + '/public/res'));
 app.use('/', express.static(__dirname + '/public/'));
