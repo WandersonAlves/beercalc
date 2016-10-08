cf8336e76c28b599a9f302a6104297a34e5f42a8
Popoto <wandersonalvesferreira2012@gmail.com>
2016-10-08 03:18:56 -0300

    Documentation

--- controllers/loading-screen.controller.js
+++ controllers/loading-screen.controller.js
@@ -1,5 +1,8 @@
-window.loading_screen = window.pleaseWait({
-	logo: "res/logo.png",
-	backgroundColor: '#FF5722',
-	loadingHtml: ""
-});
+/**
+ * @ngdoc function
+ * @name loading_screen
+ * @description
+ * Method that closes the initial splash screen
+ *
+ */
+window.loading_screen = window.pleaseWait({logo: "res/logo.png", backgroundColor: '#FF5722', loadingHtml: ""});
--- controllers/navigation.controller.js
+++ controllers/navigation.controller.js
@@ -1,5 +1,15 @@
+// TODO Complex documentation
 (function () {
-	angular.module('beercalc').controller('NavigationController', NavigationController);
+	/**
+	 * @ngdoc controller
+	 * @name beercalc.controller:NavigationController
+	 * @description
+	 * Controller that controlls how screen handles everything
+	 *
+	 */
+	angular
+		.module('beercalc')
+		.controller('NavigationController', NavigationController);
 
 	NavigationController.$inject = ['$scope', '$timeout', '$mdSidenav', '$state', 'SideMenuFactory'];
 
@@ -9,7 +19,9 @@
 		vm.toggle = toggle;
 		vm.goto = goto;
 		// NOTE Public attrs
+		// FIXME Set currentState value dynamically
 		vm.currentState = 'Home';
+		// TODO Get currentUser value from userService
 		vm.currentUser = {
 			avatar: 'res/photos/profile.jpg',
 			name: 'Wanderson Alves Ferreira',
@@ -20,11 +32,23 @@
 		vm.currExp = 62;
 
 		vm.sideMenuOptions = SideMenuFactory.constructSideMenu();
-
+		/**
+		 * @ngdoc function
+		 * @name beercalc.NavigationController:toggle
+		 * @description
+		 * Open or closes the navigation menu
+		 *
+		 */
 		function toggle() {
 			$mdSidenav('left').toggle();
 		}
-
+		/**
+		 * @ngdoc function
+		 * @name beercalc.NavigationController:goto
+		 * @description
+		 * Change the actual state route of screen
+		 *
+		 */
 		function goto(state) {
 			$state.go(state);
 			toggle();
