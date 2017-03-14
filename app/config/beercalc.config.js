8c1a8a99b95ba405a68b4d9ed83f76e44f36c950
WandersonAlves <wanderson.alves@accenture.com>
2017-03-14 16:59:28 -0300

    Various configs for Auth0.init

--- app/config/beercalc.config.js
+++ app/config/beercalc.config.js
@@ -7,7 +7,17 @@
 		$mdThemingProvider.theme('default').primaryPalette('deep-orange').dark();
 		lockProvider.init({
       clientID: 'l1OFijv6yxRnOEHZyiaKXOj7iRJKnm2r',
-      domain: 'beercalc.auth0.com'
+      domain: 'beercalc.auth0.com',
+			options: {
+				theme: {
+					logo: 'res/logo.png',
+					primaryColor: '#FF5722'
+				},
+				language: 'pt-br',
+        languageDictionary: {
+          title: "BeerCalc!"
+        }
+      }
     });
 	}
 })();
